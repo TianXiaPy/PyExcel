92d744d7a5ce6cd49b7ec3a84ad7e7313b617062
TianXiaPy <17600299187@163.com>
2020-12-13 18:02:56 +0800

    批量修改

---
+++ src/Chapter-4使用python批量处理工作簿表/批量重名名一个工作簿中的所有工作表.py
@@ -0,0 +1,17 @@
+"""
+Chapter example3 使用python批量重命名一个工作簿中的所有工作表
+Function  :
+Author    : chen yi hao
+CopyRight : TianXiaPy
+Date      : 2020-12-06
+"""
+import xlwings as xw
+app = xw.App(visible=False, add_book=False)
+workbook = app.books.open("销售表.xlsx")
+worksheets = workbook.sheets
+for wsheet in worksheets:
+    wsheet.name = wsheet.name.replace("销售", "")
+workbook.save("销售表backup.xlsx")
+# workbook.save("销售表.xlsx")
+workbook.close()
+app.quit()
---
+++ src/Chapter-4使用python批量处理工作簿表/批量重命名一个工作簿中的部分表.py
@@ -0,0 +1,15 @@
+"""
+Chapter example4 批量重命名多个工作簿
+Function  :
+Author    : chen yi hao
+CopyRight : TianXiaPy
+Date      : 2020-12-06
+"""
+import xlwings as xw
+app = xw.App(visible=False, add_book=False)
+workbook = app.books.open("销售表.xlsx")
+worksheet = workbook.sheets
+for i in range(len(worksheet))[:3]:
+    worksheet[i].name = worksheet[i].name.replace("销售", "")
+workbook.save("销售表new.xlsx")
+app.quit()

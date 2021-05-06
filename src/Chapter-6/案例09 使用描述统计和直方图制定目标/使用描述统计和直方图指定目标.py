8d902d9532bdd498625660ec0bde729142378b22
TianXiaPy <17600299187@163.com>
2021-05-06 21:29:12 +0800

    chapter 6 expample 10

--- src/Chapter-6/案例09 使用描述统计和直方图制定目标/使用描述统计和直方图指定目标.py
+++ src/Chapter-6/案例09 使用描述统计和直方图制定目标/使用描述统计和直方图指定目标.py
@@ -52,3 +52,50 @@ worksheet.autofit()
 workbook.save("描述统计1.xlsx")
 workbook.close()
 app.quit()
+"""
+cut()时pandas模块的函数，用于对数据进行离散化处理，也就是将数据从最大值到最小值进行
+等间距划分。
+cut(x,bins, right=True, labels=None, retbins=False, precision=3,
+    include_lowest=False)
+x: 一维数组
+bins: 如果为整数，表示将x划分为多少个等间距的区间；如果为序列，表示将x换份在指定
+      的序列中
+right： 设置区间是否包含右断点
+labels: 为划分出的区间指定名称标签
+retbins: 设置是否返回每个区间的端点值
+precision: 设置端点值的精度
+include_lowest: 设置区间是否包含左端点
+
+reset_index()时DataFrame对象的函数,用于重置DataFrame对象的索引
+reset_index(level=None, drop=False, inplace=False, col_level=0,col_fill="")
+level: 控制要重置哪个等级的索引
+drop: 默认为False, 表示索引列会被还原为普通列，否则会丢失
+inplace: 默认为False， 表示不修改原有的DataFrame，而创建新的
+col_level: 当列有多个级别时，用于确定将列表签插入哪个级别。默认值为0，表示插入第1层级
+col_fill: 当列有多个级别时，用于确定如何重命名其他级别。默认为""，如果为None， 则重复使用索引名
+
+figure()是matplotlib.pyplot模块中的函数，用于创建一个绘图窗口。
+figure(num=None, figsize=None,dpi=None, facecolor=None, edgecolor=None,
+       frameon=True, clear=False)
+num: 可选，设置窗口名称，默认为None
+figsize: 可选，设置窗口大小，默认为None
+dpi: 可选，设置窗口分辨率，默认为None
+facecolor: 可选，设置窗口的背景颜色
+edgecolor: 可选，用于设置窗口的边框颜色
+frameon: 可选，表示是否绘制窗口的图框，如果为False，则绘制窗口的图框
+clear: 可选，如果为True并且窗口中已经有图形，则清除该窗口中的图形
+
+
+hist()函数是Matplotlib模块中的函数，用于绘制直方图。该函数的语法格式和常用
+参数如下：
+hist(x,bins=None,range=None,density=False,
+     color=None,edgecolor=None,linewidth=None)
+x: 指定用于绘制直方图的数据
+bins：如果为整数，表示将数据等分为相应数量的区间，默认值为10；
+     如果为序列，表示用序列的元素作为区间的端点值
+range：指定参与分组统计的数据范围，不在此范围内的数据将被忽略。如果
+     参数bins取值为序列形式，则此参数无效
+density：如果为True,表示绘制频率直方图；如果为False,表示绘制频数直方图
+color/edgecolor/inewidth: 分别用于设置主子的填充颜色，边框颜色，边框粗细
+
+"""
---
+++ src/Chapter-6/案例10 使用回归分析预测未来值/使用回归分析预测未来值.py
@@ -0,0 +1,36 @@
+"""
+Chapter6 example10 使用回归分析预测未来值
+Function  :
+Author    : chen yi hao
+CopyRight : TianXiaPy
+Date      : 2021-05-06
+
+老王：
+下图为某公司2019年每月的汽车销售额和在两种渠道投入的广告费，
+如果现在需要根据广告费来预测销售额，你会怎么做呢？
+
+小新：
+我知道使用Excel的数据分析工具
+中的“回归”功能可以拟合出线性回归方程，但是如何判断拟合
+出的方程是否可靠，在Python中又该怎么编程呢？
+
+老王：
+要判断方程是否可靠，需要通过计算R2值来判断方程的拟合程度。在Python中，
+使用sklearn模块的LinearRegression()函数可
+以快速拟合出线性回归方程，使用score()函数可以计算R2值。下面
+就来看看如何在拟合出方程后计算R2值
+"""
+import pandas as pd  # 导入pandas模块
+from sklearn import linear_model  # 导入sklearn模块
+
+df = pd.read_excel("回归分析.xlsx", header=None)  # 读取指定工作簿中的数据
+df = df[2:]  # 删除前两行数据
+df.columns = ["月份", "电视台广告费", "视频门户广告费", "汽车当月销售额"]
+x = df[['视频门户广告费', '电视台广告费']]  # 获取“视频门户广告费”列和“电视台广告费”列的数据作为自变量
+y = df["汽车当月销售额"]  # 获取销售额列的数据作为因变量
+model = linear_model.LinearRegression()  # 创建一个线性回归模型
+print(x)
+print(y)
+model.fit(x, y)  # 用自变量和因变量对回归模型进行训练，拟合出线性回归方程
+R2 = model.score(x, y)  # 计算R2值
+print(R2)
